5c100c23e0727b45bf55a07d9384f61f65c8d00d
Cong Luan Tran <congluan.tran19@gmail.com>
2026-04-21 16:05:13 +0700

    Replace socket with socketserver

--- app/main.py
+++ app/main.py
@@ -1,24 +1,33 @@
-import socket  # noqa: F401
+import socketserver
+
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        while True:
+            data = self.rfile.readline(10000)
+            if not data:
+                break
+
+            data = data.rstrip()
+            if data == b"PING":
+                self.wfile.write(b"+PONG\r\n")
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
+    HOST, PORT = "localhost", 6379
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            else:
-                lines = data.splitlines()
-                for line in lines:
-                    if line == b"PING":
-                        conn.sendall(b"+PONG\r\n")
+    # Bind must happen after allow_reuse_adress for the setting to work
+    with socketserver.ThreadingTCPServer(
+        (HOST, PORT), MyTCPHandler, bind_and_activate=False
+    ) as server:
+        server.allow_reuse_address = True
+        server.server_bind()
+        server.server_activate()
+        print(f"serving on {HOST} as port {PORT}")
+        server.serve_forever()
 
 
 if __name__ == "__main__":
